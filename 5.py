6299162f3920e6e05a96a8795555e13f807dee60
shifelfs <50615759+shifelfs@users.noreply.github.com>
2019-05-15 12:01:07 +0530

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,9 +1,9 @@
-a=int(input())
-b=int(input())
-c=int(input())
+a=float(input())
+b=float(input())
+c=float(input())
 if(a>b and a>c):
 	print(a)
-elif(b>c):
+elif(b>c and b>a):
 	print(b)
 else:
 	print(c)
