8d8972de5e02fbf60e03a7c9933ef72a7618edde
shifelfs <50615759+shifelfs@users.noreply.github.com>
2019-05-15 11:25:33 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,7 +1,7 @@
 inp=str(input())
 if((inp>='a' and inp<='z') or (inp>='A' and inp<='Z')):
 	if(inp=='a' or inp=='e' or inp=='i' or inp=='o' or inp=='u' or inp=='A' or inp=='E' or inp=='I' or inp=='O' or inp=='U'):
-		print("Vowels")
+		print("Vowel")
 	else:
 		print("Consonant")
 else:
