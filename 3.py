536c2f4c58b6440396ca3f6154fd21067a91befe
shifelfs <50615759+shifelfs@users.noreply.github.com>
2019-05-15 11:18:12 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,8 @@
+inp=str(input())
+if((inp>='a' and inp<='z') or (inp>='A' and inp<='Z')):
+	if(inp=='a' or inp=='e' or inp=='i' or inp=='o' or inp=='u' or inp=='A' or inp=='E' or inp=='I' or inp=='O' or inp=='U'):
+		print("Vowels")
+	else:
+		print("Consonant")
+else:
+	print("invalid")
