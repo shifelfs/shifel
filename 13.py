00056ae7dcb4524a0a7b106dd689b67d85f8587e
shifelfs <50615759+shifelfs@users.noreply.github.com>
2019-06-01 11:39:02 +0530

    Update 13.py

--- 13.py
+++ 13.py
@@ -2,7 +2,7 @@ num = int(input())
 if num > 1:  
    for i in range(2, num//2): 
        if (num % i) == 0: 
-           print("yes") 
+           print("no") 
            break
 else: 
-   print("no") 
+   print("yes") 
