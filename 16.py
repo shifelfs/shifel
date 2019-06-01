f3e436a20e2ef245423bd773a9e729d7eff958c3
shifelfs <50615759+shifelfs@users.noreply.github.com>
2019-06-01 12:25:20 +0530

    Add files via upload

---
+++ 16.py
@@ -0,0 +1,13 @@
+a,b=input().split()
+a=int(a)
+b=int(b)
+for num in range(a+1,b):
+	for j in range(2,num):
+           
+		if (num % j) == 0:
+              
+			break
+       
+	else:
+          
+ 		print(num,end=" ")
