d86e47832914e9151b75e5dfedc5213dbb2fe733
Shifel Malar FS <50615759+shifelfs@users.noreply.github.com>
2019-06-05 22:39:48 +0530

    Update p6.py

--- p6.py
+++ p6.py
@@ -1,9 +1,9 @@
 n=int(input())
 l=list(map(int,input().split()))
 count=0
-for i in range(len(l)):
-    for j in range(i+1,len(l)):
+for i in range(len(l)-2):
+    for j in range(i+1,len(l)-1):
         for k in range(j+1,len(l)):
-            if z[i]<z[j]<z[k] and i<j<k:
+            if l[i]<l[j]<l[k] and i<j<k:
                 count=count+1
 print(count)
