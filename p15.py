e893bc64fcfe255307d4893bac5017f8e6e16c2a
Shifel Malar FS <50615759+shifelfs@users.noreply.github.com>
2019-06-06 10:50:38 +0530

    Update p15.py

--- p15.py
+++ p15.py
@@ -1,7 +1,9 @@
-z=int(input())
-l=[int(i) for i in input().split()]
-l=[bin(i)[2:] for i in  l]
-l.sort(reverse=True)
-m=[int(i,2) for i in l]
-for i in m:
-	print(i)
+h=int(input())
+l=list(map(int,input().split()))
+m1=[]
+for i in l:
+	s=bin(i)[2::]
+	m1.append(([s.count("1"),i]))
+m1.sort(reverse=True)
+for i in range(0,len(m1)):
+	print(m1[i][1])
