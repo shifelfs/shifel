00b2892fff04354e38db416d64511e66ce89a6c7
Shifel Malar FS <50615759+shifelfs@users.noreply.github.com>
2019-06-13 16:00:00 +0530

    Update a21.py

--- a21.py
+++ a21.py
@@ -4,11 +4,11 @@ b=input().split()
 
 c=input().split()
 
-if a[0]==b[0]==c[0]:
+if a[0]==b[0]==c[0] or a[1]==b[1]==c[1]:
   
 	print('yes')
 
-elif a[1]==b[1]==c[1]:
+elif a[0]==a[1] or b[0]==b[1] or c[0]==c[1]:
 
 	print('yes')
 
