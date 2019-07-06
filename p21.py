6671c29af2cceb96446738736f131182370a8c0c
Shifel Malar FS <50615759+shifelfs@users.noreply.github.com>
2019-07-06 19:31:36 +0530

    Delete p21.py

--- p21.py
+++
@@ -1,9 +0,0 @@
-a=int(input())
-l=list(map(int,input().split()))
-avg=int(a/2)
-list1=l[:avg]
-list2=l[avg::]
-if ((sum(list1)//len(list1))==(sum(list2)//len(list2))):
-    print("yes")
-else:
-    print("no")
