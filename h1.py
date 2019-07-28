4daa0a659ab6469b746a9dcf44dd2d8f4cc08a5d
Shifel Malar FS <50615759+shifelfs@users.noreply.github.com>
2019-07-28 20:05:23 +0530

    Add files via upload

---
+++ h1.py
@@ -0,0 +1,14 @@
+n=int(input())
+l=list(map(int,input().split()))
+a=[]
+b=[]
+s=set(l)
+if len(l)==len(s):
+	print("unique")
+else:
+	for i in range(len(l)):
+		if l[i] not in a:
+			a.append(l[i])
+		else:
+			b.append(l[i])
+print (*b)
