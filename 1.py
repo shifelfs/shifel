0792b5eb5a228c61daff35880b4c8b101cbbf1ce
Shifel Malar FS <50615759+shifelfs@users.noreply.github.com>
2019-08-20 20:33:46 -0700

    Add files via upload
    to cancatenate the numbers given as input and print the maximum no of occurence of that  number and the number

--- 1.py
+++ 1.py
@@ -1,7 +1,16 @@
-a=float(input())
-if(a==0):
-	print("Zero")
-elif(a>=0):
-	print("Positive")
-else:
-	print("Negative")
+//print the max occurence of number and the number by concatenating the list
+l=list(input().split())
+a=""
+b=[]
+for i in l:
+  a=a+i
+for i in range(0,10):
+  i=str(i)
+  c=a.count(i)
+  b.append(c)
+m=max(b)
+print(m)
+for i in range(0,10):
+  if b[i]==m:
+    print(i,end=" ")
+  
